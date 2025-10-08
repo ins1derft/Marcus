6884ea93c6543ddfa2a4e6bcf2340c2fdd90b962
Danila <105750709+ins1derft@users.noreply.github.com>
2025-10-08 16:24:23 +0400

    Adjust Strapi CSP for S3 assets

--- strapi/config/middlewares.ts
+++ strapi/config/middlewares.ts
@@ -1,12 +1,36 @@
-export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
-];
+export default ({ env }) => {
+  const hosts = ['https://market-assets.strapi.io'];
+
+  const s3CdnBaseUrl = env('S3_CDN_BASE_URL');
+  const s3Endpoint = env('S3_ENDPOINT');
+
+  if (s3CdnBaseUrl) {
+    hosts.push(s3CdnBaseUrl.replace(/\/$/, ''));
+  } else if (s3Endpoint) {
+    hosts.push(s3Endpoint.replace(/\/$/, ''));
+  }
+
+  return [
+    'strapi::logger',
+    'strapi::errors',
+    {
+      name: 'strapi::security',
+      config: {
+        contentSecurityPolicy: {
+          useDefaults: true,
+          directives: {
+            'img-src': ["'self'", 'data:', 'blob:', ...hosts],
+            'media-src': [...hosts],
+          },
+        },
+      },
+    },
+    'strapi::cors',
+    'strapi::poweredBy',
+    'strapi::query',
+    'strapi::body',
+    'strapi::session',
+    'strapi::favicon',
+    'strapi::public',
+  ];
+};
